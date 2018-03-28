c2a6fc9f179d3a1cbafbd57e3e333538ece54c7c
Drup <drupyog@zoho.com>
2018-03-29 00:34:15 +0200

    Add tests for re.

---
+++ test/re/test_re.ml
@@ -0,0 +1,35 @@
+open Regenerate
+
+let () = Printexc.record_backtrace true
+
+let rec to_re = function
+  | One -> Re.epsilon
+  | Set l -> Re.set @@ CCString.of_list l
+  | Seq (re, re') -> Re.seq [to_re re; to_re re']
+  | Or (re, re') -> Re.alt [to_re re; to_re re']
+  | And (re, re') -> Re.inter [to_re re; to_re re']
+  | Not re -> assert false
+  | Rep (i,j,re) -> Re.repn (to_re re) i j
+
+let f (re, pos, neg) =
+  let cre =
+    try
+      Re.compile @@ Re.whole_string @@ to_re re
+    with _ -> QCheck.assume_fail ()
+  in
+  List.for_all (fun s -> Re.execp cre s) pos &&
+  List.for_all (fun s -> not @@ Re.execp cre s) neg
+
+let test =
+  let sigma = ['a'; 'b'; 'c'] in
+  let module W = Word.String in
+  let module S = Segments.ThunkList(W) in
+  let a =
+    arbitrary
+      (module W) (module S)
+      ~compl:false ~pp:Fmt.char ~samples:10
+      sigma
+  in
+  QCheck.Test.make a f
+
+let () = QCheck_runner.run_tests_main [test]
