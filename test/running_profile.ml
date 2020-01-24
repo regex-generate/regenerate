8775882b69dfc44e0a42957fa1abf1d300cdd6ea
Drup <drupyog@zoho.com>
2020-01-24 17:40:40 +0100

    Fix the benchmarking code.

--- test/running_profile.ml
+++ test/running_profile.ml
@@ -4,7 +4,7 @@ let f i l =
   let file = Fmt.strf "re_trie_%i.csv" i in
   let oc = open_out file in
   let n = 
-    L.gen sigma l
+    L.gen l
     |> Test.measure_until
       ~limit:(Mtime.Span.of_uint64_ns (5_000_000_000L))
       ~interval:20
